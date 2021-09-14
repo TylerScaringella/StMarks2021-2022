54f603b1b1c52ac0c77aaf66e5f57192f7bb7269
TylerScaringella <=>
2021-09-14 19:17:42 -0400

    Complete FirstFile.java

---
+++ First/src/me/tyler/github/FirstFile.java
@@ -0,0 +1,8 @@
+package me.tyler.github;
+
+public class FirstFile {
+
+    public static void main(String[] args) {
+        System.out.println("Hi my name is Tyler Scaringella. I am from Holliston, MA and I have an older brother.");
+    }
+}
---
+++ LinkedList/src/me/tyler/linkedlist/LinkedList.java
@@ -0,0 +1,55 @@
+package me.tyler.linkedlist;
+
+public class LinkedList<E> {
+
+    private class Node {
+
+        Node next;
+        E data;
+
+        public Node(E data) {
+            this.data = data;
+        }
+
+    }
+
+    private Node first;
+
+    public void add(E info, int i) {
+        Node node = new Node(info);
+
+        if(this.first == null) {
+            this.first = node;
+        }
+
+        if(i == 0) {
+            node.next = this.first;
+            this.first = node;
+        }
+
+        try {
+            Node curr = first;
+            for(int x = 0; x < i; x++) {
+                curr = curr.next;
+            }
+
+            node.next = curr.next;
+            curr.next = node;
+
+        } catch(NullPointerException e) {
+            throw new IndexOutOfBoundsException();
+        }
+    }
+
+    public void remove(E e) {
+
+    }
+
+    public void remove(int i) {
+
+    }
+
+    public int size() {
+        return 0;
+    }
+}
