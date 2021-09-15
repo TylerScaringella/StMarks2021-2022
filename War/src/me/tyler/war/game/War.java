0ad01d6eefbca188f6635acff568685e25f2be46
TylerScaringella <=>
2021-09-15 12:03:51 -0400

    okn war work but its not real

--- War/src/me/tyler/war/game/War.java
+++ War/src/me/tyler/war/game/War.java
@@ -2,10 +2,12 @@ package me.tyler.war.game;
 
 import me.tyler.war.util.LinkedList;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class War {
 
@@ -74,8 +76,9 @@ public class War {
     }
 
     private void startRounds() {
+        AtomicInteger round = new AtomicInteger(1);
         while(this.playerOne.size() != 0 || this.playerTwo.size() != 0) {
-            System.out.println("Player 1 has " + this.playerOne.size() + " cards. Player 2 has " + this.playerTwo.size() + " cards.\n");
+            System.out.println("(Round " + round.getAndIncrement() + ") Player 1 has " + this.playerOne.size() + " cards. Player 2 has " + this.playerTwo.size() + " cards.\n");
 
             System.out.println("Press enter to deal cards.");
             this.scanner.nextLine();
@@ -93,9 +96,11 @@ public class War {
         System.out.println("Player 1 plays the " + playerOneCard.getNumber() + " of " + playerOneCard.getSuit());
         System.out.println("Player 2 plays the " + playerTwoCard.getNumber() + " of " + playerTwoCard.getSuit());
         if(playerOneCard.getNumber() == playerTwoCard.getNumber()) {
-            System.out.println("Draw! Both players get their cards back (temp)");
-            this.playerOne.add(playerOneCard);
-            this.playerTwo.add(playerTwoCard);
+            System.out.println("War!");
+            this.handleWar(Arrays.asList(
+                    playerOneCard,
+                    playerTwoCard
+            ));
         } else if(playerOneCard.getNumber() > playerTwoCard.getNumber()){
             System.out.println("Player 1 wins the round!");
             this.playerOne.add(playerOneCard);
@@ -104,6 +109,31 @@ public class War {
             System.out.println("Player 2 wins the round!");
             this.playerTwo.add(playerOneCard);
             this.playerTwo.add(playerTwoCard);
+        }
+    }
+
+    // fake war doesnt actually work (it does but its not the real thing)
+    private void handleWar(List<Card> ogCards) {
+        List<Card> cards = new ArrayList<>(ogCards);
+
+        Card playerOneCard = this.handleCard(this.playerOne);
+        Card playerTwoCard = this.handleCard(this.playerTwo);
+
+        cards.add(playerOneCard);
+        cards.add(playerTwoCard);
+
+        System.out.println("Player 1 plays the " + playerOneCard.getNumber() + " of " + playerOneCard.getSuit());
+        System.out.println("Player 2 plays the " + playerTwoCard.getNumber() + " of " + playerTwoCard.getSuit());
+
+        if(playerOneCard.getNumber() == playerTwoCard.getNumber()) {
+            System.out.println("War!");
+            this.handleWar(cards);
+        } else if(playerOneCard.getNumber() > playerTwoCard.getNumber()){
+            System.out.println("Player 1 wins the war!");
+            cards.forEach(this.playerOne::add);
+        } else {
+            System.out.println("Player 2 wins the war!");
+            cards.forEach(this.playerTwo::add);
         }
     }
 
