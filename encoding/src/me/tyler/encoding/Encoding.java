a5cd0f62c7b01f4a965a8e0fd4c519d303306ad1
TylerScaringella <=>
2021-10-12 13:30:32 -0400

    Working encoding assignment

--- encoding/src/me/tyler/encoding/Encoding.java
+++ encoding/src/me/tyler/encoding/Encoding.java
@@ -1,6 +1,7 @@
 package me.tyler.encoding;
 
 import java.io.*;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -28,6 +29,7 @@ public class Encoding {
             int charNum = -1;
             while((charNum = reader.read()) != -1) {
                 char readChar = (char) charNum;
+                // We want to keep track of the frequency of every character, so we have a map of a character and the amount of times it has been seen in the file
                 int freq = this.freqMap.getOrDefault(readChar, 0);
                 freq++;
                 this.freqMap.put(readChar, freq);
@@ -38,8 +40,10 @@ public class Encoding {
         }
 
         PriorityQueue<Branch<Character>> queue = new PriorityQueue<>();
+        // Go through the frequency map that we have adding to a priority queue with branches with a priority based on the frequency of the character
         this.freqMap.forEach((character, freq) -> queue.add(new Branch(character), freq));
 
+        // We want to turn the priority queue into a binary tree, so we only want there to be 1 entry
         while(queue.size() > 1) {
             if(queue.size() >= 2) {
                 // Branch with children
@@ -61,6 +65,7 @@ public class Encoding {
             }
         }
 
+        // Initiate the codes of characters in our binary tree
         this.buildCode("", queue.pop());
 
         this.writeCodes();
@@ -69,9 +74,11 @@ public class Encoding {
     }
 
     public void buildCode(String currentCode, Branch<Character> currentBranch) {
+        // If a branch is a leaf then we know that there are no more children
         if(currentBranch.isLeaf()) {
             this.codeMap.put(currentBranch.getInfo(), currentCode);
         }else {
+            // When a branch has children, the branch's children may have children so we want to get the values of the children
             this.buildCode(currentCode + "0", currentBranch.getLeft());
             this.buildCode(currentCode + "1", currentBranch.getRight());
         }
@@ -83,7 +90,12 @@ public class Encoding {
             this.codeMap.forEach((character, code) -> {
                 try {
                     // This will write our codes into our codes.txt file in the format "(char):code:(code)"
-                    writer.write(character + ":code:" + code + "\n");
+                    if(((int) character) <= 31 || ((int) character) == 127) {
+                        // This will write integers in addition to characters if the character provided is an empty character that way we can read it properly
+                        writer.write(character + "" + (int)character + ":code:" + code + "\n");
+                    } else {
+                        writer.write(character + ":code:" + code + "\n");
+                    }
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
@@ -126,8 +138,18 @@ public class Encoding {
 
             while((line = reader.readLine()) != null) {
                 // This right here is reversing our writing for codes.txt |
+                // If the line is empty, then we know that the character is empty, meaning we are going to have the get the character value of the integer provided
+                if(line.isEmpty()) {
+                    String nextLine = reader.readLine();
+                    String[] split = nextLine.split(":code:");
+                    char character = (char) Integer.parseInt(split[0]);
+                    String code = split[1];
+                    this.charMap.put(code, character);
+                    continue;
+                }
+
                 String[] split = line.split(":code:");
-                char character = split[0].toCharArray()[0];
+                char character = split[0].charAt(0);
                 String code = split[1];
                 this.charMap.put(code, character);
             }
@@ -139,6 +161,7 @@ public class Encoding {
 
         String cur = "";
 
+        // This will go through reading each and every character we have stored in our map and constantly check if the group of bits is stored anywhere in our map, and if it is, adding that character
         while(reader.hasNext()) {
             boolean bit = reader.readBit();
             cur += (bit ? "1" : "0");
